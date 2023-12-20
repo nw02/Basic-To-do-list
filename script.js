ef888534793afc95ae5e4af15d2c6d10beb3b123
Vinicius <vofreitas2.0@gmail.com>
2023-12-20 15:39:09 -0300

    LocalStorage added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function addTask() {
     console.log(`A tarefa está armazenada, e é : ${NewTask}`)
 
     // Creates a list item that receive the value of input
-    var Item = document.createElement('li')
+    let Item = document.createElement('li')
     Item.textContent = NewTask
 
     // Creates a delete button that will be inside list item
@@ -13,7 +13,8 @@ function addTask() {
     deleteButton.id = 'deleteButton';
     deleteButton.textContent = 'Delete';
     deleteButton.onclick = function() {
-        List.removeChild(Item)
+        List.removeChild(Item) 
+        updateLocalStorage(); 
     }
 
     // Makes delete stay inside on item, and item inside the list
@@ -21,6 +22,10 @@ function addTask() {
     List.appendChild(Item);
 
     // After action be executed the input will be empty
+    document.getElementById("TypeTask").value = '';
+
+    // Update localStorage after add a task
+    updateLocalStorage();
 }
 
 document.getElementById("TypeTask").addEventListener("keydown", function(event) {
@@ -29,3 +34,47 @@ document.getElementById("TypeTask").addEventListener("keydown", function(event) 
         addTask();
     }
 });
+
+function updateLocalStorage() {
+    var taskItems = document.getElementById("TaskList").getElementsByTagName("li");
+    var tasks = []
+
+    // add text of each item of list on array
+    for(var i = 0; i < taskItems.length; i++){
+        tasks.push(taskItems[i].textContent.trim())
+    }
+    
+    // Stores the array in localStorage as a JSON string
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasksFromLocalStorage() {
+    var storedTasks = localStorage.getItem('tasks');
+
+    if(storedTasks){
+        // transforms the JSON back into an array
+        var tasks = JSON.parse(storedTasks);
+        console.log(tasks)
+
+        var List = document.getElementById("TaskList");
+
+        for(var i = 0; i < tasks.length; i++) {
+            let Item = document.createElement("li")
+            Item.textContent = tasks[i].replace("Delete", "");
+
+            var deleteButton = document.createElement('button');
+            deleteButton.id = 'deleteButton';
+            deleteButton.textContent = 'Delete';
+
+            deleteButton.onclick = function() {
+                List.removeChild(Item);
+                updateLocalStorage();
+            };
+
+            Item.appendChild(deleteButton);
+            List.appendChild(Item);
+        }
+    }
+}
+
+loadTasksFromLocalStorage();
